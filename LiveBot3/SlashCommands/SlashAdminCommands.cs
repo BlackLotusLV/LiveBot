b4237a0e40059992c39ecfb0da2bc5b11607f72f
Tomass <melnaislotoss@gmail.com>
2022-05-08 21:20:05 +0300

    Removed a commented out code section that is scrapped

--- LiveBot3/SlashCommands/SlashAdminCommands.cs
+++ LiveBot3/SlashCommands/SlashAdminCommands.cs
@@ -166,37 +166,5 @@ namespace LiveBot.SlashCommands
                 await response.Result.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("FAQ message edited").AsEphemeral());
             }
         }
-        /*
-        [SlashCommand("news", "Posts news article to the news channel")]
-        [SlashRequireGuild]
-        public async Task News(InteractionContext ctx)
-        {
-            var modal = new DiscordInteractionResponseBuilder().WithTitle("News Post Form").WithCustomId($"modal-{ctx.User.Id}")
-            .AddComponents(new TextInputComponent(label: "News Title", customId: "Title", value: ""))
-            .AddComponents(new TextInputComponent(label: "Body Text", customId: "Body", value: ""))
-            .AddComponents(new TextInputComponent(label: "Media link", customId: "Media", value: "",required: false));
-
-            await ctx.CreateResponseAsync(InteractionResponseType.Modal, modal);
-            var interactivity = ctx.Client.GetInteractivity();
-            var response = await interactivity.WaitForModalAsync($"modal-{ctx.User.Id}", ctx.User);
-            if (!response.TimedOut)
-            {
-                var modalInteraction = response.Result.Interaction;
-                var values = response.Result.Values;
-                DiscordEmbedBuilder embed = new()
-                {
-                    Author = new DiscordEmbedBuilder.EmbedAuthor()
-                    {
-                        Name = ctx.Client.CurrentUser.Username,
-                        IconUrl = ctx.Client.CurrentUser.AvatarUrl
-                    },
-                    Title = values["Title"],
-                    Description = values["Body"],
-                    Color= new DiscordColor(0x59bfff)
-                };
-                await modalInteraction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed));
-            }
-        }
-        //*/
     }
 }
