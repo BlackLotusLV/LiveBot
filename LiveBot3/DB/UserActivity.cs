5a38fbf91bd8ef70aa03a67fbf46a5c8398c6825
Tomass <melnaislotoss@gmail.com>
2022-08-27 12:37:17 +0300

    Added user activity data base tables

---
+++ LiveBot3/DB/UserActivity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LiveBot.DB
+{
+    [Table("User_Activity", Schema = "livebot")]
+    internal class UserActivity
+    {
+        [Key]
+        [Column("id_user_activity")]
+        public long ID_User_Activity { get; set; }
+
+        [Required]
+        [Column("user_id")]
+        public ulong User_ID
+        { get => _User_ID; set { _User_ID = Convert.ToUInt64(value); } }
+        private ulong _User_ID;
+
+        [Required]
+        [Column("points")]
+        public int Points { get; set; }
+
+        [Required]
+        [Column("date")]
+        public DateTime Date { get; set; }
+    }
+}
