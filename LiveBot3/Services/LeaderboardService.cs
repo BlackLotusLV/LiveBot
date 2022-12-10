33ef6d83f2fdb98890ed7bedf3c8e25fe8a1a4bc
Tomass <melnaislotoss@gmail.com>
2022-12-10 14:41:28 +0200

    Improved stability and performance of leaderboard service

--- LiveBot3/Services/LeaderboardService.cs
+++ LiveBot3/Services/LeaderboardService.cs
@@ -4,15 +4,36 @@ namespace LiveBot.Services
 {
     internal static class LeaderboardService
     {
-        private static readonly ConcurrentQueue<LeaderboardItem> _leaderboard = new();
+        // Use a concurrent bag instead of a queue to avoid locking and improve performance
+        private static readonly ConcurrentBag<LeaderboardItem> _leaderboard = new();
+
+        // Use a CancellationTokenSource and CancellationToken to be able to stop the thread
+        private static readonly CancellationTokenSource cts = new();
+        private static readonly CancellationToken token = cts.Token;
 
         private static readonly Thread thread = new(() =>
         {
-            while (_leaderboard.TryDequeue(out LeaderboardItem item))
+            // Check if cancellation has been requested
+            while (!token.IsCancellationRequested)
             {
-                AddToServerLeaderboard(item.User, item.Guild);
+                try
+                {
+                    if (_leaderboard.IsEmpty)
+                    {
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+                    if (_leaderboard.TryTake(out LeaderboardItem item))
+                    {
+                        AddToServerLeaderboard(item.User, item.Guild);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Program.Client.Logger.LogError(CustomLogEvents.LiveBot,"Leaderboard Service experienced an error\n{exceptionMessage}", ex.Message);
+                }
             }
-            Thread.Sleep(100);
         });
 
         public static void StartService()
@@ -21,9 +42,20 @@ namespace LiveBot.Services
             Program.Client.Logger.LogInformation(CustomLogEvents.LiveBot, "Leaderboard service started!");
         }
 
+        public static void StopService()
+        {
+            // Request cancellation of the thread
+            cts.Cancel();
+
+            // Wait for the thread to finish
+            thread.Join();
+
+            Program.Client.Logger.LogInformation(CustomLogEvents.LiveBot, "Leaderboard service stopped!");
+        }
+
         public static void QueueLeaderboardItem(DiscordUser user, DiscordGuild guild)
         {
-            _leaderboard.Enqueue(new LeaderboardItem(user, guild));
+            _leaderboard.Add(new LeaderboardItem(user, guild));
         }
 
         public static void AddUserToLeaderboard(DiscordUser user, string locale)
@@ -33,7 +65,7 @@ namespace LiveBot.Services
             DB.Leaderboard newEntry = new()
             {
                 ID_User = user.Id,
-                Locale= locale
+                Locale = locale
             };
             DB.DBLists.InsertLeaderboard(newEntry);
         }
