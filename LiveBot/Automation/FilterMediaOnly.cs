73f61892f916a4006163fc0b0fe3a0de230182c8
Tomass <melnaislotoss@gmail.com>
2023-09-17 12:54:46 +0300

    moved message checks before db requests, to reduce traffic

--- LiveBot/Automation/FilterMediaOnly.cs
+++ LiveBot/Automation/FilterMediaOnly.cs
@@ -10,27 +10,27 @@ public class FilterMediaOnly
     {
         _contextFactory = contextFactory;
     }
-    public async Task OnMessageSent(DiscordClient client, MessageCreateEventArgs args)
+    public async Task OnMessageSent(DiscordClient client, MessageCreateEventArgs eventArgs)
     {
-        if (args.Guild is null) return;
+        if (eventArgs.Guild is null || eventArgs.Author.IsBot || eventArgs.Message.Attachments.Count!=0 || eventArgs.Message.Content.Split(' ').Any(x=>Uri.TryCreate(x, UriKind.Absolute, out _))) return;
         LiveBotDbContext context = await _contextFactory.CreateDbContextAsync();
-        Guild guild = await context.Guilds.Include(x => x.MediaOnlyChannels).FirstOrDefaultAsync(x => x.Id == args.Guild.Id);
+        Guild guild = await context.Guilds.Include(x => x.MediaOnlyChannels).FirstOrDefaultAsync(x => x.Id == eventArgs.Guild.Id);
         if (guild is null)
         {
-            await context.AddAsync(new Guild(args.Guild.Id));
+            await context.AddAsync(new Guild(eventArgs.Guild.Id));
             await context.SaveChangesAsync();
             return;
         }
-        if (guild.MediaOnlyChannels.Count == 0 || args.Author.IsBot || args.Message.Attachments.Count!=0 || args.Message.Content.Split(' ').Any(x=>Uri.TryCreate(x, UriKind.Absolute, out _))) return;
-        MediaOnlyChannels channel = guild.MediaOnlyChannels.FirstOrDefault(x => x.ChannelId == args.Channel.Id);
+        if (guild.MediaOnlyChannels.Count == 0) return;
+        MediaOnlyChannels channel = guild.MediaOnlyChannels.FirstOrDefault(x => x.ChannelId == eventArgs.Channel.Id);
         if (channel is null) return;
-        await args.Message.DeleteAsync();
+        await eventArgs.Message.DeleteAsync();
         string response = channel.ResponseMessage ?? "This channel is for sharing media only, please use the appropriate channel for discussions. If this is a mistake please contact a moderator.";
         
-        DiscordMessage msg = await args.Channel.SendMessageAsync(response);
+        DiscordMessage msg = await eventArgs.Channel.SendMessageAsync(response);
         await Task.Delay(9000);
         await msg.DeleteAsync();
         client.Logger.LogInformation(CustomLogEvents.PhotoCleanup,
-            "User {Username}({UserId}) tried to send text in a media only channel. Message deleted", args.Author.Username, args.Author.Id);
+            "User {Username}({UserId}) tried to send text in a media only channel. Message deleted", eventArgs.Author.Username, eventArgs.Author.Id);
     }
 }
